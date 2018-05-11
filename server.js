ef4182a01a45bccc76dc9c8a3a18f208156a54ee
Eric J <34559118+jiapurdue@users.noreply.github.com>
2018-05-10 20:39:11 -0400

    change PORT

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
